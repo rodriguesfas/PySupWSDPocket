508bbc5763d750799acc95b72733f8bd1169170f
Jayr <jayralencarpereira@gmail.com>
2020-11-25 22:12:22 -0300

    dynamic model path

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import json
 
 sentence = 'The human brain is quite proficient at word-sense disambiguation.'
 
-nlp = PySupWSDPocket(lang='en', model='semcor_omsti')
+nlp = PySupWSDPocket(lang='en', model='semcor_omsti', model_path="/home/USER/pysupwsdpocket_models")
 doc = nlp.wsd(raw_text=sentence)
 
 print(doc)
--- pysupwsdpocket/pysupwsdpocket.py
+++ pysupwsdpocket/pysupwsdpocket.py
@@ -7,18 +7,20 @@ from os import path
 
 class PySupWSDPocket(object):
 
-    def __init__(self, lang, model):
+    def __init__(self, lang, model, model_path = None):
         self.lang = lang
         self.model = model
 
-        self.HOME = os.environ['HOME']
+        if model_path is None:
+            self.WORKSPACE = os.environ['HOME'] + '/pysupwsdpocket_models'
+        else:
+            self.WORKSPACE = model_path
 
     def wsd(self, raw_text):
         HERE = path.abspath(path.dirname(__file__))
         JAR_FILE = HERE+'/supwsd-pocket.jar'
-        WORKSPACE = self.HOME + '/pysupwsdpocket_models'
 
-        args = [raw_text, self.lang, self.model, WORKSPACE]
+        args = [raw_text, self.lang, self.model, self.WORKSPACE]
 
         try:
             doc = subprocess.check_output(['java', '-jar', JAR_FILE, *args], shell=False)
